0169bb41601dfc0c52b6b5b783d9bcaa100b404f
Libor Svejda <Libor.Svejda@unicorn.com>
2019-10-13 22:40:49 +0200

    Různá funkčnost podle url požadavku (/appinfo)

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const http = require('http');
 
 const PORT = 8888;
+const VERSION = "0.0.1";
 
 let numReq = 0;
 
@@ -11,8 +12,13 @@ http.createServer((req, res) => {
         return;
     }
     numReq++;
-    res.writeHead(200, {"Content-type": "text/html"});
-    res.end(`<html><meta charset="UTF-8"><body>Počet požadavků: ${numReq}</body></html>`);
+    if (req.url === "/appinfo") {
+        res.writeHead(200, {"Content-type": "text/html"});
+        res.end(`<html><meta charset="UTF-8"><body><h1>První servřík</h1>Verze: ${VERSION}</body></html>`);
+    } else {
+        res.writeHead(200, {"Content-type": "text/html"});
+        res.end(`<html><meta charset="UTF-8"><body>Počet požadavků: ${numReq}</body></html>`);
+    }
 }).listen(PORT);
 
 console.log(`Server běží na adrese http://localhost:${PORT}...`);
