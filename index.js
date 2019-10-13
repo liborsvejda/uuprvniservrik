6e8e3407da582a13358b41bc4d5bc80a53c3197f
Libor Svejda <Libor.Svejda@unicorn.com>
2019-10-13 22:42:50 +0200

    Různá funkčnost podle url požadavku (/api)

--- index.js
+++ index.js
@@ -12,7 +12,19 @@ http.createServer((req, res) => {
         return;
     }
     numReq++;
-    if (req.url === "/appinfo") {
+    if (req.url.startsWith("/api/")) {
+        res.writeHead(200, {"Content-type": "application/json"});
+        let obj = {};
+        if (req.url === "/api/appinfo") {
+            obj.version = VERSION;
+            obj.author = "Libor Švejda";
+        } else if (req.url === "/api/reqnum") {
+            obj.numberOfRequests = numReq;
+        } else {
+            obj.error = "API not found";
+        }
+        res.end(JSON.stringify(obj));
+    } else if (req.url === "/appinfo") {
         res.writeHead(200, {"Content-type": "text/html"});
         res.end(`<html><meta charset="UTF-8"><body><h1>První servřík</h1>Verze: ${VERSION}</body></html>`);
     } else {
