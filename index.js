313c40b7b6795c60c015e3c7c1fa977c62006cb8
Libor Svejda <Libor.Svejda@unicorn.com>
2019-10-13 16:04:05 +0200

    Jen test spusteni JS

---
+++ index.js
@@ -0,0 +1,4 @@
+let c1 = 5;
+let c2 = 3;
+let soucet = c1 + c2;
+console.log(`Součet čísel ${c1} a ${c2} je ${soucet}.`); //řetězec ohraničený zpětnými apostrofy lze kombinovat s hodnotami proměnných
