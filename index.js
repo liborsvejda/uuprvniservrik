e2bc231d4fc48b7b5ed0aa42d385ddbe89581071
Libor Svejda <Libor.Svejda@unicorn.com>
2019-10-13 16:11:58 +0200

    Zobrazení počtu volání stránky

--- index.js
+++ index.js
@@ -2,9 +2,17 @@ const http = require('http');
 
 const PORT = 8888;
 
+let numReq = 0;
+
 http.createServer((req, res) => {
+    if (req.url === "/favicon.ico") {
+        res.writeHead(404);
+        res.end();
+        return;
+    }
+    numReq++;
     res.writeHead(200, {"Content-type": "text/html"});
-    res.end("<html><body>Kuk!</body></html>");
+    res.end(`<html><meta charset="UTF-8"><body>Počet požadavků: ${numReq}</body></html>`);
 }).listen(PORT);
 
 console.log(`Server běží na adrese http://localhost:${PORT}...`);
