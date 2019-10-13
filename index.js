5fecb5ba766146b20c00ccbed70eac9d87dff14b
Libor Svejda <Libor.Svejda@unicorn.com>
2019-10-13 23:13:47 +0200

    Předání souborů do výstupu (zpracování obrázků, pdf a dalších dokumentů uložených v samostatných souborech)

--- index.js
+++ index.js
@@ -1,10 +1,50 @@
 const http = require('http');
 const fs = require('fs');
+const url = require('url');
 
 const PORT = 8888;
 const VERSION = "0.0.1";
 
 let numReq = 0;
+function fileRequested(req, res) {
+    let q = url.parse(req.url, true);
+    let fileName = q.pathname;
+    if (fileName.indexOf(".") < 0 || fileName.length - fileName.indexOf(".") > 6) {
+        return false;
+    }
+    if (fileName.charAt(0) === '/') {
+        fileName = fileName.substr(1);
+    }
+    if (!fs.existsSync(fileName)) {
+        console.log("### not exists");
+        // res.writeHead(404);
+        // res.end();
+        return false;
+    }
+    let contentType = "application/octet-stream";
+    if (fileName.toLowerCase().endsWith(".html")) {
+        contentType = "text/html";
+    } else if (fileName.toLowerCase().endsWith(".jpg") || fileName.endsWith(".jpeg")) {
+        contentType = "image/jpeg";
+    } else if (fileName.toLowerCase().endsWith(".png")) {
+        contentType = "image/png";
+    } else if (fileName.toLowerCase().endsWith(".pdf")) {
+        contentType = "application/pdf"
+    };
+    let file = fs.createReadStream(fileName);
+    file.on('open', function () {
+        res.writeHead(200, {'Content-Type': contentType});
+        file.pipe(res);
+    });
+    file.on('end', function () {
+        res.end();
+    });
+    file.on('error', function () {
+        res.writeHead(500);
+        res.end();
+    });
+    return true;
+}
 
 http.createServer((req, res) => {
     if (req.url === "/favicon.ico") {
@@ -19,6 +59,8 @@ http.createServer((req, res) => {
             res.write(data);
             res.end();
         });
+    } else if (fileRequested(req, res)) {
+        return;
     } else if (req.url.startsWith("/api/")) {
         res.writeHead(200, {
             "Content-type": "application/json",
